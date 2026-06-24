f6a7c9889a597eae277d0d0628968ca87e547e14
Baeza Hector <baezahectorj@gmail.com>
2026-06-24 18:26:50 +0000

    Drafted 3NF schema and added db_handler logic

---
+++ database/db_handler.py
@@ -0,0 +1,19 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_db_connection():
+  return mysql.connector.connect(
+    host = os.getenv("DB_HOST"),
+    user = os.getenv("DB_USER"),
+    password = os.getenv("DB_PASSWORD"),
+    database = os.getenv("DB_NAME")
+  )
+
+if __name__ == "__main__":
+    conn = get_db_connection()
+    if conn.is_connected():
+        print("[System] Successfully connected to MySQL database.")
+        conn.close()
