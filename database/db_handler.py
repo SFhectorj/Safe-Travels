394cf7403751c15c888f66c2752da5ce071be7e7
Baeza Hector <baezahectorj@gmail.com>
2026-06-25 00:20:12 +0000

    Added log_route and get_route_history function to build dashboard with past entries

--- database/db_handler.py
+++ database/db_handler.py
@@ -36,6 +36,52 @@ def get_saved_location(user_id, alias):
   else:
     return alias
 
+def log_route(user_id, end_location, hazards_avoided):
+  '''
+  Records a completed route in the route history table,
+  including the destination, number of hazards avoided,
+  and the timestamp of the route.
+  '''
+  conn = get_db_connection()
+  cursor = conn.cursor()
+  
+  query = """
+        INSERT INTO route_history (user_id, end_location, hazards_avoided, log_date) 
+        VALUES (%s, %s, %s, NOW());
+        """
+
+  cursor.execute(query, (user_id, end_location, hazards_avoided))
+  conn.commit()
+
+  cursor.close()
+  conn.close()
+
+def get_route_history(user_id):
+  '''
+  Fetch previous routes for the dashboard
+  Retrieves the five most recent routes completed by a user,
+  including destination, hazards avoided, and route date.
+  '''
+  conn = get_db_connection()
+  cursor = conn.cursor(dictionary=True)
+
+  query = """
+        SELECT route_id, end_location, hazards_avoided, DATE(log_date) as date 
+        FROM route_history 
+        WHERE user_id = %s 
+        ORDER BY log_date DESC LIMIT 5;
+        """
+
+  cursor.execute(query, (user_id,))  # comma creates single element tuple
+  results = cursor.fetchall()
+
+  cursor.close()
+  conn.close()
+
+  return results
+
+
+
 if __name__ == "__main__":
     conn = get_db_connection()
     if conn.is_connected():
